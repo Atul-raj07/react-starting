944a8c88f3ef0cdd7a86e3d1b4b65c09de2bfcad
pratiksha harode <pratikshaharode0@gmail.com>
2024-09-18 18:25:04 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Card from "./components/Card";
-function App(){
+function App()
+{
   return (
-  <>
+    <>
     <Card />
-  </>
-)
+    </>
+     )
 }
 
 export default App;
